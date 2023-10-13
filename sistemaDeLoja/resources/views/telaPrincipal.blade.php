0b07c28198bb0f52f2a174bf21610b3730961c5e
Daniel Berg <105886779+danielberglopes@users.noreply.github.com>
2023-10-13 09:07:30 -0300

    chore
    Atualizacao

---
+++ sistemaDeLoja/resources/views/telaPrincipal.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.main')
+
+@section('css')
+<style>
+    #bloco{
+
+        background-color: #e9ecef;
+		-webkit-box-shadow: 9px 7px 5px rgba(50, 50, 50, 0.77);
+		-moz-box-shadow:    9px 7px 5px rgba(50, 50, 50, 0.77);
+		box-shadow:         9px 7px 5px rgba(50, 50, 50, 0.77);
+        margin: 5rem;  height: 16rem;   
+	}
+</style>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" 
+integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  </head>
+@endsection
+@section('content')
+
+
+
+
+<div class="container text-center" style="margin-top: -10rem;">
+  <div class="row gy-6">
+    <div class="col-6">
+      <div id="bloco" class="p-3" >Custom column padding</div>
+    </div>
+    <div class="col-6">
+      <div id="bloco" class="p-3">Custom column padding</div>
+    </div>
+    <div class="col-6">
+      <div id="bloco" class="p-3" style="margin-top: 4rem; ">Custom column padding</div>
+    </div>
+    <div class="col-6">
+      <div  id="bloco" class="p-3" style="margin-top: 4rem;">Custom column padding</div>
+    </div>
+  </div>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" 
+integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+ crossorigin="anonymous"></script>
+@endsection
